7fc155f5166c03616220508970280591a82f3d3b
mbogochow <mbogochow@hotmail.com>
2013-05-18 14:17:56 -0400

    File uploading changes to app.js

--- app.js
+++ app.js
@@ -38,7 +38,18 @@ app.get("/", store.home, function(req, res) {
 // list of items
 app.get("/items", store.items);
 // show an individual item
-app.get("item/:id", store.item);
+app.get("/item/:id", function(req, res){
+  res.send('<form method="post" id="uploadForm" enctype="multipart/form-data" action="post">'
+    //+ '<p>Title: <input type="text" name="title" /></p>'
+    + '<p>Upload File: <input type="file" id="userInput" name="userFile" /></p>'
+    //+ '<p><input type="submit" value="Upload" /></p>'
+    + '</form>'
+    + '<span id="status" />'
+    + '<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>'
+    + '<script src="/javascripts/jquery.form.js"></script>'
+    + '<script src="/javascripts/upload.js"></script>'
+    );
+});
 
 // show general pages
 app.get("/page", store.page);
@@ -48,6 +59,29 @@ app.get('/logout', function(req, res) {
     delete req.session.username;
     // redirect user to homepage
     res.redirect('/');
+});
+
+app.post('/item/post', function(req, res) {
+  var serverPath = '/files/' + req.files.userFile.name;
+ //console.log(process.cwd() + '/public');
+  require('fs').rename(
+    req.files.userFile.path,
+    process.cwd() + '/public' + 
+    //'C:/Users/mbogochow/Documents/School/Hackademy/Hackathon/fileUpload/public' + 
+    serverPath,
+    function(error) {
+      if(error) {
+        res.send({
+          error: 'Something bad happened  in app.js app.post fs.rename'
+        });
+        return;
+      }
+   
+      res.send({
+        path: serverPath
+      });
+    }
+  );
 });
 
 app.post("/", store.home_post_handler);
